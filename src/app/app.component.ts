fdf23fab138d0f94b5613783dec2a007bd5dc577
Bram Borggreve <borggreve@gmail.com>
2017-04-01 16:01:46 -0500

    Implement some simple logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { sum } from '../lib/sum';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public result = sum(1, 2);
 }
